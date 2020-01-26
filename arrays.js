2274356aaeeb3dd7a48832ae7b00bc062366f84b
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-01-26 17:22:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skitels"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
