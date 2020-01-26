c96c080f84b65db7989d8e7da7660e3d60434663
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-01-26 18:31:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,21 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
